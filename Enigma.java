02a7b24cb72ba2476d278c294fb5151cddea66a8
rlyyah <tmakowski1337@gmail.com>
2020-02-05 18:40:01 +0100

    ciphering class

--- Enigma.java
+++ Enigma.java
@@ -1,12 +1,18 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Enigma {
 
     public String cipher5 = "abcdefghijklmnopqrstuvwxyz";
     public String cipher6 = "bacedfhgikjlnmoqprtsuwvxzy";
     public String cipher7 = "zyxwvutsrqponmlkjihgfedcba";
+
+
+    private String inicial; 
+    private String cipherEven;
+    private String cipherOdd;
+
     public List<String> c5;
     public List<String> c6;
     public List<String> c7;
@@ -19,32 +25,60 @@ public class Enigma {
         return input;
     }
 
+    public String cipherSentence(String sentence){
+        
+        String ciphered = "";
+        String cipheredLetter = "";
+        
+        sentence = sentence.replaceAll("\\s+","");
 
-
-    public Enigma(){
-        c5 = new ArrayList();
-        c6 = new ArrayList();
-        c7 = new ArrayList();
-        for(int i=0; i<cipher5.length();i++){
-            c5.add(String.valueOf(cipher5.charAt(i)));
-            c6.add(String.valueOf(cipher6.charAt(i)));
-            c7.add(String.valueOf(cipher7.charAt(i)));
+        for(int i=0;i<sentence.length();i++){
+            if(i%2==0){
+                cipheredLetter = String.valueOf(cipher7.charAt(cipher5.indexOf(String.valueOf(sentence.charAt(i)))));
+            }else{
+                cipheredLetter = String.valueOf(cipher6.charAt(cipher5.indexOf(String.valueOf(sentence.charAt(i)))));
+            }
+            ciphered += cipheredLetter;
         }
 
+        return ciphered;
+    }
 
+
+
+    public Enigma(Integer key, String Cipher){
 
     }
-    
-    public static void main(String[] args){
-        for(int i=0;i<args.length;i++){
-            System.out.println(args[i]);
-        }
-        Enigma enigma = new Enigma();
-        for(String s: enigma.c5){
-            System.out.println(s);
-        }
 
     
+    public static void main(String[] args){
+        String mode = "";
+        
+            try{
+                mode = args[0];
+            }catch(Exception e){
+                System.out.println("exception!");
+                mode = "-c";
+            }
+        Integer key = 123;
+        String cipher = "AAG";
+
+        Enigma enigma = new Enigma(key, cipher);
+        
+        String userInput = null;
+
+        while(userInput != ("q")){
+
+            userInput = enigma.getInput();
+
+            if(String.valueOf(mode.charAt(1))=="d"){
+                
+                System.out.println(enigma.decipherSentence(userInput));
+            }else{
+                System.out.println(enigma.cipherSentence(userInput));
+            }
+        }
+        
         
     }
 }
