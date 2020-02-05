ff1ac347f118ae37bcaa989e1227e057dcb400e9
rlyyah <tmakowski1337@gmail.com>
2020-02-05 23:04:56 +0100

    Add ability to choose key and cipher

--- Enigma.java
+++ Enigma.java
@@ -34,9 +34,9 @@ public class Enigma {
 
         for(int i=0;i<sentence.length();i++){
             if(i%2==0){
-                cipheredLetter = String.valueOf(cipher7.charAt(cipher5.indexOf(String.valueOf(sentence.charAt(i)))));
+                cipheredLetter = String.valueOf(cipherEven.charAt(inicial.indexOf(String.valueOf(sentence.charAt(i)))));
             }else{
-                cipheredLetter = String.valueOf(cipher6.charAt(cipher5.indexOf(String.valueOf(sentence.charAt(i)))));
+                cipheredLetter = String.valueOf(cipherOdd.charAt(inicial.indexOf(String.valueOf(sentence.charAt(i)))));
             }
             ciphered += cipheredLetter;
         }
@@ -53,9 +53,9 @@ public class Enigma {
 
         for( int i=0;i<sentence.length();i++){
             if (i%2==0){
-                decipheredLetter = String.valueOf(cipher5.charAt(cipher7.indexOf(String.valueOf(sentence.charAt(i)))));
+                decipheredLetter = String.valueOf(inicial.charAt(cipherEven.indexOf(String.valueOf(sentence.charAt(i)))));
             }else{
-                decipheredLetter = String.valueOf(cipher5.charAt(cipher6.indexOf(String.valueOf(sentence.charAt(i)))));
+                decipheredLetter = String.valueOf(inicial.charAt(cipherOdd.indexOf(String.valueOf(sentence.charAt(i)))));
             }
             
             deciphered += decipheredLetter;
@@ -64,22 +64,98 @@ public class Enigma {
         return deciphered;
     }
 
-    public Enigma(Integer key, String Cipher){
+    public Enigma(Integer key, String cipher){
+        switch(key){
+            case 123:
+            inicial = cipher5;
+            cipherEven = cipher6;
+            cipherOdd = cipher7;
+            break;
+            case 132:
+            inicial = cipher5;
+            cipherEven = cipher7;
+            cipherOdd = cipher6;
+            break;
+            case 213:
+            inicial = cipher6;
+            cipherEven = cipher5;
+            cipherOdd = cipher7;
+            break;
+            case 231:
+            inicial = cipher6;
+            cipherEven = cipher7;
+            cipherOdd = cipher5;
+            break;
+            case 312:
+            inicial = cipher7;
+            cipherEven = cipher5;
+            cipherOdd = cipher6;
+            break;
+            case 321:
+            inicial = cipher7;
+            cipherEven = cipher6;
+            cipherOdd = cipher5;
+            break;
+            default:
+            inicial = cipher5;
+            cipherEven = cipher6;
+            cipherOdd = cipher7;
+        }
+    String inicialHelp = "";
 
+    for(int i = 0; i<inicial.length();i++){
+        if(i<inicial.length()-inicial.indexOf(String.valueOf(cipher.charAt(0)))){
+            inicialHelp += String.valueOf(inicial.charAt(i+inicial.indexOf(String.valueOf(cipher.charAt(0)))));
+        }else{
+            inicialHelp += String.valueOf(inicial.charAt(i-(inicial.length()-inicial.indexOf(String.valueOf(cipher.charAt(0))))));
+        }
+    }
+    inicial = inicialHelp;
+
+    String cipherEvenHelp = "";
+
+    for(int i = 0; i<cipherEven.length();i++){
+        if(i<cipherEven.length()-cipherEven.indexOf(String.valueOf(cipher.charAt(1)))){
+            cipherEvenHelp += String.valueOf(cipherEven.charAt(i+cipherEven.indexOf(String.valueOf(cipher.charAt(1)))));
+        }else{
+            cipherEvenHelp += String.valueOf(cipherEven.charAt(i-(cipherEven.length()-cipherEven.indexOf(String.valueOf(cipher.charAt(1))))));
+        }
+    }
+    cipherEven = cipherEvenHelp;
+
+    String cipherOddHelp = "";
+    for(int i = 0; i<cipherOdd.length();i++){
+        if(i<cipherOdd.length()-cipherOdd.indexOf(String.valueOf(cipher.charAt(2)))){
+            cipherOddHelp += String.valueOf(cipherOdd.charAt(i+cipherOdd.indexOf(String.valueOf(cipher.charAt(2)))));
+        }else{
+            cipherOddHelp += String.valueOf(cipherOdd.charAt(i-(cipherOdd.length()-cipherOdd.indexOf(String.valueOf(cipher.charAt(2))))));
+        }
+    }
+    cipherOdd = cipherOddHelp;
+    
+    System.out.println(inicial);
+    System.out.println(cipherEven);
+    System.out.println(cipherOdd);
+    
     }
 
     
     public static void main(String[] args){
         String mode = "";
+        Integer key = 123;
+        String cipher = "ddd";
         
             try{
                 mode = args[0];
+                key = Integer.parseInt(args[1]);
+                cipher = args[2];
             }catch(Exception e){
                 System.out.println("exception!");
+                key = 123;
                 mode = "-c";
+                cipher = "abz";
             }
-        Integer key = 123;
-        String cipher = "AAG";
+        
 
         Enigma enigma = new Enigma(key, cipher);
         
@@ -89,11 +165,13 @@ public class Enigma {
 
             userInput = enigma.getInput();
 
-            if(String.valueOf(mode.charAt(1))=="d"){
-                
+            if(String.valueOf(mode.charAt(1)).equals("d")){
+                System.out.println("Deciphation completed:");
                 System.out.println(enigma.decipherSentence(userInput));
             }else{
+                System.out.println("Ciphation completed:");
                 System.out.println(enigma.cipherSentence(userInput));
+                System.out.println(enigma.decipherSentence(enigma.cipherSentence(userInput)));
             }
         }
         
